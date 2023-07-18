d97fbe1952549184deff4d46f6670999ada84b82
Şaban Birkan Genç <139702707+SabanGnc@users.noreply.github.com>
2023-07-18 14:00:49 +0300

    Add files via upload

---
+++ Usb Duplicator/Usb Duplicator/Usb Duplicator/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Usb_Duplicator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.Animated = true;
+            this.guna2Button1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button1.BorderColor = System.Drawing.Color.Orange;
+            this.guna2Button1.BorderRadius = 15;
+            this.guna2Button1.BorderThickness = 3;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.DarkOrange;
+            this.guna2Button1.Font = new System.Drawing.Font("LEMON MILK Bold", 20F, System.Drawing.FontStyle.Bold);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(346, 336);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(267, 109);
+            this.guna2Button1.TabIndex = 0;
+            this.guna2Button1.Text = "C O P Y";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(964, 549);
+            this.Controls.Add(this.guna2Button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+    }
+}
+
---
+++ Usb Duplicator/Usb Duplicator/Usb Duplicator/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Usb_Duplicator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            // USB cihazlarının bağlı olduğu sürücüleri al
+            string[] driveList = Directory.GetLogicalDrives();
+
+            // İlk USB sürücüsünü bul
+            string sourceDrive = "";
+            foreach (string drive in driveList)
+            {
+                DriveInfo driveInfo = new DriveInfo(drive);
+                if (driveInfo.DriveType == DriveType.Removable)
+                {
+                    sourceDrive = drive;
+                    break;
+                }
+            }
+
+            // İlk USB sürücüsü bulunamazsa hata mesajı göster
+            if (sourceDrive == "")
+            {
+                MessageBox.Show("USB cihazı bulunamadı!");
+                return;
+            }
+
+            // İkinci USB sürücüsünü bul
+            string destinationDrive = "";
+            foreach (string drive in driveList)
+            {
+                DriveInfo driveInfo = new DriveInfo(drive);
+                if (driveInfo.DriveType == DriveType.Removable && drive != sourceDrive)
+                {
+                    destinationDrive = drive;
+                    break;
+                }
+            }
+
+            // İkinci USB sürücüsü bulunamazsa hata mesajı göster
+            if (destinationDrive == "")
+            {
+                MessageBox.Show("İkinci USB cihazı bulunamadı!");
+                return;
+            }
+
+            // Kaynak ve hedef dizinleri oluştur
+            string sourceDirectory = sourceDrive + "\\";
+            string destinationDirectory = destinationDrive + "\\";
+
+            // Kaynak dizinindeki dosyaları ve alt klasörleri hedef dizine kopyala
+            try
+            {
+                CopyFiles(sourceDirectory, destinationDirectory);
+
+                MessageBox.Show("Dosyalar başarıyla kopyalandı!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu: " + ex.Message);
+            }
+        }
+
+        private void CopyFiles(string sourceDir, string destinationDir)
+        {
+            Directory.CreateDirectory(destinationDir);
+
+            // Kaynak dizinindeki dosyaları kopyala
+            foreach (string file in Directory.GetFiles(sourceDir))
+            {
+                string fileName = Path.GetFileName(file);
+                string destinationFile = Path.Combine(destinationDir, fileName);
+                File.Copy(file, destinationFile, true);
+            }
+
+            // Kaynak dizinindeki alt klasörleri dolaşarak kopyala
+            foreach (string subDir in Directory.GetDirectories(sourceDir))
+            {
+                string dirName = Path.GetFileName(subDir);
+                string destinationSubDir = Path.Combine(destinationDir, dirName);
+                CopyFiles(subDir, destinationSubDir);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Formun ekranın ortasında konumlanması için gereken hesaplamaları yap
+            int x = (Screen.PrimaryScreen.Bounds.Width - this.Width) / 2;
+            int y = (Screen.PrimaryScreen.Bounds.Height - this.Height) / 2;
+
+            // Formun konumunu güncelle
+            this.Location = new Point(x, y);
+
+            //-------------------------------------------
+
+            // Formun adını değiştir
+            this.Text = "USB DUPLICATOR";
+        }
+    }
+}
